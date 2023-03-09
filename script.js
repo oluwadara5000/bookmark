09c0e66a93a82ad8c8d33dd7806d53f25f975fd2
Oluwadarasimi Odejobi <oluwadarasimi5000@gmail.com>
2023-03-09 22:19:07 +0100

    done with main screen design. Time for media queries

--- script.js
+++ script.js
@@ -6,8 +6,30 @@ const showClick = document.getElementById("show-click");
 const showSearch = document.getElementById("show-search");
 const showShare = document.getElementById("show-share");
 
+
+const showBookmark = document.getElementById("bookmark-icon-show")
+const showBrowser = document.getElementById("browser-icon-show")
+const showApp = document.getElementById("app-icon-show")
+const showChromium = document.getElementById("chromium-icon-show");
+
+const hideBookmark = document.getElementById("bookmark-icon-close");
+const hideBrowser = document.getElementById("browser-icon-close");
+const hideApp = document.getElementById("app-icon-close");
+const hideChromium = document.getElementById("chromium-icon-close");
+
+
+const bookmarkParagraph = document.getElementById("bookmark-p");
+const browserParagraph = document.getElementById("browser-p");
+const appParagraph = document.getElementById("app-p");
+const chromiumParagraph = document.getElementById("chromium-p");
+
+showClick.classList.add("add-border");
 search.style.display = "none";
 share.style.display = "none";
+hideBookmark.style.display = "none";
+hideBrowser.style.display = "none";
+hideApp.style.display = "none";
+hideChromium.style.display = "none";
 
 showClick.addEventListener("click", function () {
   search.style.display = "none";
@@ -36,4 +58,90 @@ showShare.addEventListener("click", function () {
   showClick.classList.remove("add-border");
   showSearch.classList.remove("add-border");
   showShare.classList.add("add-border");
+});
+
+
+showBookmark.addEventListener("click", function () {
+  bookmarkParagraph.style.display = "block"
+  browserParagraph.style.display = "none"
+  appParagraph.style.display = "none"
+  chromiumParagraph.style.display = "none";
+  showBookmark.style.display = "none"
+  hideBookmark.style.display = "block"
+  hideBrowser.style.display = "none"
+  hideApp.style.display = "none"
+  hideChromium.style.display = "none";
+  showBrowser.style.display = "block";
+  showApp.style.display = "block";
+  showChromium.style.display = "block";
+});
+
+showBrowser.addEventListener("click", function () {
+  bookmarkParagraph.style.display = "none";
+  browserParagraph.style.display = "block";
+  appParagraph.style.display = "none";
+  chromiumParagraph.style.display = "none";
+  showBrowser.style.display = "none";
+  hideBrowser.style.display = "block";
+  hideBookmark.style.display = "none";
+  hideApp.style.display = "none";
+  hideChromium.style.display = "none";
+  showBookmark.style.display = "block";
+  showApp.style.display = "block";
+  showChromium.style.display = "block";
+});
+
+showApp.addEventListener("click", function () {
+  bookmarkParagraph.style.display = "none";
+  browserParagraph.style.display = "none";
+  appParagraph.style.display = "block";
+  chromiumParagraph.style.display = "none";
+  showApp.style.display = "none";
+  hideApp.style.display = "block";
+  hideBrowser.style.display = "none";
+  hideBookmark.style.display = "none";
+  hideChromium.style.display = "none";
+  showBrowser.style.display = "block";
+  showBookmark.style.display = "block";
+  showChromium.style.display = "block";
+});
+
+showChromium.addEventListener("click", function () {
+  bookmarkParagraph.style.display = "none";
+  browserParagraph.style.display = "none";
+  appParagraph.style.display = "none";
+  chromiumParagraph.style.display = "block";
+  showChromium.style.display = "none";
+  hideChromium.style.display = "block";
+  hideBrowser.style.display = "none";
+  hideApp.style.display = "none";
+  hideBookmark.style.display = "none";
+  showBrowser.style.display = "block";
+  showApp.style.display = "block";
+  showBookmark.style.display = "block";
+});
+
+
+hideBookmark.addEventListener("click", function () {
+  bookmarkParagraph.style.display = "none"
+  hideBookmark.style.display = "none"
+  showBookmark.style.display = "block"
+});
+
+hideBrowser.addEventListener("click", function () {
+  browserParagraph.style.display = "none";
+  hideBrowser.style.display = "none";
+  showBrowser.style.display = "block";
+});
+
+hideApp.addEventListener("click", function () {
+  appParagraph.style.display = "none";
+  hideApp.style.display = "none";
+  showApp.style.display = "block";
+});
+
+hideChromium.addEventListener("click", function () {
+  chromiumParagraph.style.display = "none";
+  hideChromium.style.display = "none";
+  showChromium.style.display = "block";
 });
